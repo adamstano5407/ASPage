001fa7ee4a66f5f672f49dce7cbf114c69f92d6f
Adam Staňo <adamstano5@gmail.com>
2026-07-14 14:38:43 +0200

    [API] added new migration

---
+++ api/Migrations/20260714123750_UpdateDatabaseModel.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace APIKros.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateDatabaseModel : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Id",
+                table: "UserRoles",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Id",
+                table: "UserRoles");
+        }
+    }
+}
