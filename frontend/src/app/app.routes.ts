983877e2de5b14fe46a1e5d8f98c48be35ef680e
Adam Staňo <adamstano5@gmail.com>
2026-07-22 14:20:23 +0200

    [Frontend] added new HomeComponent and animations

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,13 +1,16 @@
 import { Routes } from '@angular/router';
 import { MainLayout } from './layout/main-layout/main-layout';
+import { HomeComponent } from './shared/components/HomeComponent/home.component';
 
 export const routes: Routes = [
-    {
+  {
+    path: '',
+    component: MainLayout,
+    children: [
+      {
         path: '',
-        component: MainLayout,
-        children: [
-        
-        ]
-    }
-
+        component: HomeComponent,
+      },
+    ],
+  },
 ];
